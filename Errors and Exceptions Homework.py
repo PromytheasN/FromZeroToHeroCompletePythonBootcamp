d2ec78af4c378ff19374b59fb60bf79d9b7cfbee
PromytheasN <63561890+PromytheasN@users.noreply.github.com>
2020-05-13 12:18:07 -0700

    Update Errors and Exceptions Homework.py

--- Errors and Exceptions Homework.py
+++ Errors and Exceptions Homework.py
@@ -1,1 +1,51 @@
+"""
+Problem 1
+Handle the exception thrown by the code below by using try and except blocks.
+"""
 
+for i in ["a","b","c"]:
+    try:
+        print(i**2)
+    except:
+        print("WhoOoOps! This is not a number")
+        
+        
+"""
+Problem 2
+Handle the exception thrown by the code below by using try and except blocks. Then use a finally block to print 'All Done.'
+x = 5
+y = 0
+z = x/y
+"""
+
+x = 5
+y = 0
+try:
+    z = x/y
+except ZeroDivisionError:
+    print("It seems that your divider is zero, there for the result "\
+           "\nto the infinite and beyond!!! ")
+
+finally:
+    print("All done budy!")
+    
+"""
+Problem 3
+Write a function that asks for an integer and prints the square of it. 
+Use a while loop with a try, except, else block to account for incorrect inputs.
+"""
+
+from IPython.display import clear_output
+def int_sqr_result():
+   
+    
+    while True:
+        try:
+            number = int(input("Please provide a number that you would like to find the square of: "))
+            clear_output()
+        except:
+            print("What you provided seems that is not a number!")
+            continue
+        else:
+            print("The square of the number is: " ,number**2, "\nThank you kindly!")
+            break
