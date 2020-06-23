7178bba8fc7eecec9dad400bdf101ab6932091dd
PromytheasN <63561890+PromytheasN@users.noreply.github.com>
2020-06-23 14:44:50 -0700

    Update Milestone Project #2.py

--- Milestone Project #2.py
+++ Milestone Project #2.py
@@ -158,7 +158,27 @@ def show_all_cards(player, dealer):
     print("Dealer's Hand =", dealer.value)
     print("\nPlayer's Hand:", *player.cards, sep="\n ")
     print("Player's Hand =",player.value)
+          
+def player_busts(player,dealer,chips):
+    print("Player busts!")
+    chips.lose_bet()
 
+def player_wins(player,dealer,chips):
+    print("Player wins!")
+    chips.win_bet()
+
+def dealer_busts(player,dealer,chips):
+    print("Dealer busts!")
+    chips.win_bet()
+    
+def dealer_wins(player,dealer,chips):
+    print("Dealer wins!")
+    chips.lose_bet()
+    
+def push(player,dealer):
+    print("Dealer and Player tie! It's a push.")
+
+          
 """
 This notes are for better understanding of how to call methods within classes.     
 we can test/call it using this ways.
